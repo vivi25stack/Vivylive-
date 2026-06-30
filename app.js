a9c8f21972be503c42e807b61633dbf48ce212c6
vivi25stack <goodnessabroad@gmail.com>
2026-07-01 00:21:37 +0100

    Refactor app.js to load and display hosts

--- app.js
+++ app.js
@@ -1,19 +1,75 @@
-document.getElementById("app").innerHTML=`
+// VIVY APP
 
-<div class="splash">
+const hostList = document.getElementById("hostList");
 
-<div class="logo">
-💜
+const hosts = [
+
+{
+name:"Sophia",
+country:"🇺🇸",
+image:"https://picsum.photos/300/400?1"
+},
+
+{
+name:"Emma",
+country:"🇬🇧",
+image:"https://picsum.photos/300/400?2"
+},
+
+{
+name:"Ava",
+country:"🇨🇦",
+image:"https://picsum.photos/300/400?3"
+},
+
+{
+name:"Olivia",
+country:"🇦🇺",
+image:"https://picsum.photos/300/400?4"
+}
+
+];
+
+function loadHosts(){
+
+hostList.innerHTML="";
+
+hosts.forEach((host)=>{
+
+hostList.innerHTML += `
+
+<div class="hostCard">
+
+<img class="hostImage" src="${host.image}">
+
+<div class="hostInfo">
+
+<div class="hostName">
+
+${host.country} ${host.name}
+
 </div>
 
-<div class="title">
-VivyLive
+<div class="online">
+
+🟢 Online
+
 </div>
 
-<div class="subtitle">
-Premium Audio & Video Calling
+<button class="callBtn">
+
+Call Now
+
+</button>
+
 </div>
 
 </div>
 
 `;
+
+});
+
+}
+
+loadHosts();
