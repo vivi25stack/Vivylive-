f8185ee73e507aee0049594ac29dbd48f25b44bf
vivi25stack <goodnessabroad@gmail.com>
2026-06-30 18:20:06 +0100

    Add splash screen HTML to app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+document.getElementById("app").innerHTML=`
 
+<div class="splash">
+
+<div class="logo">
+💜
+</div>
+
+<div class="title">
+VivyLive
+</div>
+
+<div class="subtitle">
+Premium Audio & Video Calling
+</div>
+
+</div>
+
+`;
