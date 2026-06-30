78e8c7e191242464b5664fb18447c9f36568c3db
vivi25stack <goodnessabroad@gmail.com>
2026-07-01 00:18:55 +0100

    Implement user authentication functions
    Added functions for user registration, login, and logout using Firebase Authentication.

--- auth.js
+++ auth.js
@@ -1,1 +1,62 @@
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
+const auth = window.auth;
+
+// Register
+window.registerUser = async function(email,password){
+
+try{
+
+const user = await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+alert("Registration Successful");
+
+console.log(user.user.uid);
+
+}catch(err){
+
+alert(err.message);
+
+}
+
+}
+
+// Login
+window.loginUser = async function(email,password){
+
+try{
+
+const user = await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+alert("Welcome to Vivy ❤️");
+
+console.log(user.user.uid);
+
+}catch(err){
+
+alert(err.message);
+
+}
+
+}
+
+// Logout
+window.logoutUser = async function(){
+
+await signOut(auth);
+
+alert("Logged Out");
+
+  }
