92c2cfb0273f29ca7a48f3e6562ecb020eb85625
vivi25stack <goodnessabroad@gmail.com>
2026-07-01 00:30:38 +0100

    Refactor app.js to add registration and login functions

--- app.js
+++ app.js
@@ -1,75 +1,22 @@
-// VIVY APP
+function showRegister(){
 
-const hostList = document.getElementById("hostList");
-
-const hosts = [
-
-{
-name:"Sophia",
-country:"🇺🇸",
-image:"https://picsum.photos/300/400?1"
-},
-
-{
-name:"Emma",
-country:"🇬🇧",
-image:"https://picsum.photos/300/400?2"
-},
-
-{
-name:"Ava",
-country:"🇨🇦",
-image:"https://picsum.photos/300/400?3"
-},
-
-{
-name:"Olivia",
-country:"🇦🇺",
-image:"https://picsum.photos/300/400?4"
-}
-
-];
-
-function loadHosts(){
-
-hostList.innerHTML="";
-
-hosts.forEach((host)=>{
-
-hostList.innerHTML += `
-
-<div class="hostCard">
-
-<img class="hostImage" src="${host.image}">
-
-<div class="hostInfo">
-
-<div class="hostName">
-
-${host.country} ${host.name}
-
-</div>
-
-<div class="online">
-
-🟢 Online
-
-</div>
-
-<button class="callBtn">
-
-Call Now
-
-</button>
-
-</div>
-
-</div>
-
-`;
-
-});
+document.getElementById("loginPage").classList.add("hidden");
+document.getElementById("registerPage").classList.remove("hidden");
 
 }
 
-loadHosts();
+function showLogin(){
+
+document.getElementById("registerPage").classList.add("hidden");
+document.getElementById("loginPage").classList.remove("hidden");
+
+}
+
+function openHome(){
+
+document.getElementById("loginPage").classList.add("hidden");
+document.getElementById("registerPage").classList.add("hidden");
+
+document.getElementById("homePage").classList.remove("hidden");
+
+}
