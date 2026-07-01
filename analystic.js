5e4400498dbef4bd8f73faa20731a2ef1a15caf4
vivi25stack <goodnessabroad@gmail.com>
2026-07-01 17:06:04 +0100

    Add trackEvent function for user analytics

--- analystic.js
+++ analystic.js
@@ -1,1 +1,45 @@
+// ==========================================
+// VIVY ANALYTICS SYSTEM (FINAL)
+// ==========================================
 
+// Track Event
+async function trackEvent(type, value = 1) {
+
+    try {
+
+        const today = new Date().toISOString().split("T")[0];
+
+        const ref = window.doc(
+            window.db,
+            "analytics",
+            today
+        );
+
+        const snap = await window.getDoc(ref);
+
+        if (!snap.exists()) {
+
+            await window.setDoc(ref, {
+
+                date: today,
+
+                newUsers: 0,
+                totalCalls: 0,
+                totalMinutes: 0,
+                totalCoins: 0,
+                totalGifts: 0,
+                newFollowers: 0,
+
+                createdAt: new Date().toISOString()
+
+            });
+
+        }
+
+        const data = (await window.getDoc(ref)).data();
+
+        switch(type){
+
+            case "user":
+
+               
