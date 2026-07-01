b62011baac7017f183b471176323866f4cefdbdf
vivi25stack <goodnessabroad@gmail.com>
2026-07-01 16:59:11 +0100

    Add deleteDoc and addDoc functions to Firebase export

--- firebase.js
+++ firebase.js
@@ -14,8 +14,15 @@ doc,
 setDoc,
 getDoc,
 updateDoc,
+deleteDoc,
+addDoc,
 collection,
-getDocs
+getDocs,
+query,
+where,
+orderBy,
+limit,
+onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -40,17 +47,27 @@ const auth = getAuth(app);
 
 const db = getFirestore(app);
 
+// Export Firebase
 window.auth = auth;
 window.db = db;
 
+// Auth
 window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 window.signInWithEmailAndPassword = signInWithEmailAndPassword;
 window.signOut = signOut;
 window.onAuthStateChanged = onAuthStateChanged;
 
+// Firestore
 window.doc = doc;
 window.setDoc = setDoc;
 window.getDoc = getDoc;
 window.updateDoc = updateDoc;
+window.deleteDoc = deleteDoc;
+window.addDoc = addDoc;
 window.collection = collection;
 window.getDocs = getDocs;
+window.query = query;
+window.where = where;
+window.orderBy = orderBy;
+window.limit = limit;
+window.onSnapshot = onSnapshot;
